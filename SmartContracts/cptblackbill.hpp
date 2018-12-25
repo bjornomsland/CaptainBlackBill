ce45841551c731e1f9807e35160c9e9f0d1f209e
Bjørn Omsland <39827237+bjornomsland@users.noreply.github.com>
2018-12-25 05:46:55 +0100

    Create cptblackbill.hpp

---
+++ SmartContracts/cptblackbill.hpp
@@ -0,0 +1,11 @@
+/**
+ *  @file
+ *  @copyright defined in eos/LICENSE.txt
+ */
+#pragma once
+
+#include <eosiolib/eosio.hpp>
+#include <eosiolib/asset.hpp>
+#include <eosiolib/print.hpp>
+#include <eosiolib/crypto.h>
+#include <string>
