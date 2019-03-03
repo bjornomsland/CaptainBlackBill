9d7490769ee26ece6a3ff29cf36881961708ce27
Bjørn Omsland <39827237+bjornomsland@users.noreply.github.com>
2019-03-03 10:20:55 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,170 +2,192 @@ var http = require('http');
 var express = require('express');
 
 http.createServer((request, response) => {
-  
-  const { headers, method, url } = request;
-  let body = [];
-  var transactionReturn = "";
-  
-  request.on('error', (err) => {
 
-    response.writeHead(200, {'Content-Type': 'application/json'})
-    const responseBody = { 'Error': err.message };
-    response.write(JSON.stringify(transactionReturn));
-    response.end();
-  
-  }).on('data', (jsonData) => {
-    var jsonContent = JSON.parse(jsonData);    
-    console.log('Received data');
-    console.log(jsonContent);
-    var returndata = {transactions: []}; //To use as return value
-    
-    Eos = require('eosjs');
-    eos = Eos({
-        httpEndpoint: jsonContent.httpEndpoint,
-        chainId: jsonContent.chainId,
-        keyProvider: jsonContent.keyProvider,
-        expireInSeconds: 60,
-        verbose: false
-    });
+    const { headers, method, url } = request;
+    let body = [];
+    var transactionReturn = "";
 
-    //Start first transaction
-    try {
-          eos.transaction(
-            {
-               actions: [
-               {
-                  account: jsonContent.transactions[0].account,
-                  name: jsonContent.transactions[0].actionName,
-                  authorization: [{
-                     actor: jsonContent.transactions[0].actor,
-                     permission: 'active'
-                  }],
-                  data: jsonContent.transactions[0].binArgs
-                }
-                ]
-             }).then(function (value) {
-               console.log('First Transaction:');
-               var jsonTrans1Result = JSON.parse(JSON.stringify(value));
-               returndata.transactions.push({transno: "1", eostransid: jsonTrans1Result['processed']['id'], status: jsonTrans1Result['processed']['receipt']['status']});
-               
-               if(jsonContent.transactions.length > 1){
-		  //===SECOND TRANSACTION===================================================
-		   console.log('StartSecondTransaction');
-		   eos.transaction(
-            	   {
-                      actions: [
-                      {
-                        account: jsonContent.transactions[1].account,
-                        name: jsonContent.transactions[1].actionName,
-                        authorization: [{
-                          actor: jsonContent.transactions[1].actor,
-                          permission: 'active'
-                        }],
-                        data: jsonContent.transactions[1].binArgs
-                      }
-                      ]
+    request.on('error', (err) => {
+
+        response.writeHead(200, { 'Content-Type': 'application/json' })
+        const responseBody = { 'Error': err.message };
+        response.write(JSON.stringify(transactionReturn));
+        response.end();
+
+    }).on('data', (jsonData) => {
+        var jsonContent = JSON.parse(jsonData);
+        console.log('Received data');
+        console.log(jsonContent);
+        var returndata = { transactions: [] }; //To use as return value
+
+        Eos = require('eosjs');
+        eos = Eos({
+            httpEndpoint: jsonContent.httpEndpoint,
+            chainId: jsonContent.chainId,
+            keyProvider: jsonContent.keyProvider,
+            expireInSeconds: 60,
+            verbose: false
+        });
+
+        if (jsonContent.description == 'createNewAccount') {
+
+            try {
+                var Ecc = require('eosjs-ecc');
+                Ecc.randomKey().then(privateKey => {
+                    var publicKey = Ecc.privateToPublic(privateKey);
+
+                    console.log("PublicKey: " + publicKey);
+
+                    eos.transaction(tr => {
+                        tr.newaccount({
+                            creator: 'cptblackbill',
+                            name: jsonContent.newAccountName,
+                            owner: publicKey,
+                            active: publicKey
+                        })
+
+                        tr.buyrambytes({
+                            payer: 'cptblackbill',
+                            receiver: jsonContent.newAccountName,
+                            bytes: 3692
+                        })
+
+                        tr.delegatebw({
+                            from: 'cptblackbill',
+                            receiver: jsonContent.newAccountName,
+                            stake_net_quantity: jsonContent.stakeNetQuantity,
+                            stake_cpu_quantity: jsonContent.stakeCpuQuantity,
+                            transfer: 0
+                        })
+			
+			tr.transfer({
+			    from: 'cptblackbill', 
+   		            to: jsonContent.newAccountName, 
+                            quantity: jsonContent.transferEOSAmount, 
+                            memo: 'Welcome to Cpt.BlackBill.'
+			})
+
+
                     }).then(function (value) {
-                      var jsonTrans2Result = JSON.parse(JSON.stringify(value));
-                      returndata.transactions.push({transno: "2", eostransid: jsonTrans2Result['processed']['id'], status: jsonTrans2Result['processed']['receipt']['status']});
-               
-                      if(jsonContent.transactions.length > 2){
-		      //===THIRD TRANSACTION===================================================
-		        console.log('StartThirdTransaction');
-                        eos.transaction(
-            	        {
-                          actions: [
-                          {
-                            account: jsonContent.transactions[2].account,
-                            name: jsonContent.transactions[2].actionName,
-                            authorization: [{
-                              actor: jsonContent.transactions[2].actor,
-                              permission: 'active'
-                            }],
-                            data: jsonContent.transactions[2].binArgs
-                          }
-                          ]
-                        }).then(function (value) {
-                           var jsonTrans3Result = JSON.parse(JSON.stringify(value));
-                           returndata.transactions.push({transno: "3", eostransid: jsonTrans3Result['processed']['id'], status: jsonTrans3Result['processed']['receipt']['status']});
-               
-                           if(jsonContent.transactions.length > 3){
-		           //===FOURTH TRANSACTION===================================================
-		             console.log('StartFourthTransaction');
-			     eos.transaction(
-            	             {
-                               actions: [
-                               {
-                                  account: jsonContent.transactions[3].account,
-                                  name: jsonContent.transactions[3].actionName,
-                                  authorization: [{
-                                   actor: jsonContent.transactions[3].actor,
-                                   permission: 'active'
-                                  }],
-                                  data: jsonContent.transactions[3].binArgs
-                               }]
-                             }).then(function (value) {
-                                var jsonTrans4Result = JSON.parse(JSON.stringify(value));
-                                returndata.transactions.push({transno: "4", eostransid: jsonTrans4Result['processed']['id'], status: jsonTrans4Result['processed']['receipt']['status']});
-               
-                                if(jsonContent.transactions.length > 4){
-		                //===FIRTH TRANSACTION===================================================
-		                console.log('StartFifthTransaction. Not implemented.');
+                        console.log('NewAccountExecuted: ' + publicKey);
+                        var jsonTrans1Result = JSON.parse(JSON.stringify(value));
+                        returndata.transactions.push({ transno: "1", eostransid: jsonTrans1Result['processed']['id'], privateKey: privateKey, publicKey: publicKey, status: jsonTrans1Result['processed']['receipt']['status'] });
 
-		                //===ENDOFFIFTH=====================================================================
-	                        }
-                                else{
-                                  console.log('SendResponseAfterFourthTransactionFinish');
-                                  response.writeHead(200, {'Content-Type': 'application/json'})
-                                  response.write(JSON.stringify(returndata));
-                                  response.end();
-                                }
-                           });
-		           //===ENDOFFOURTH=====================================================================
-	                   }
-                           else{
-                              console.log('SendResponseAfterThirdTransactionFinish');
-                              response.writeHead(200, {'Content-Type': 'application/json'})
-                              response.write(JSON.stringify(returndata));
-                              response.end();
-                           }
-                      });
-		      //===ENDOFTHIRD=====================================================================
-	              }
-		      else{
-                           console.log('SendResponseAfterSecondTransactionFinish');
-                           response.writeHead(200, {'Content-Type': 'application/json'})
-                           response.write(JSON.stringify(returndata));
-                           response.end();
-                      }
-                   });
-		  //===ENDOFSECOND=====================================================================
-	          }
-                  else{
-                      console.log('SendResponseAfterFirstTransactionFinish');
-		      response.writeHead(200, {'Content-Type': 'application/json'})
-                      response.write(JSON.stringify(returndata));
-                      response.end();
-                  }
-               }
-           );
-    }catch (e) {
-       console.log('FirstTransactionFailed');
-       console.log(e);
-    }  
+                        console.log('SendResponse');
+                        response.writeHead(200, { 'Content-Type': 'application/json' })
+                        response.write(JSON.stringify(returndata));
+                        response.end();
+                    });
+                
+                });
+            } catch (e) {
+                console.log('TransactionFailed');
+                returndata.transactions.push({ transno: "1", eostransid: '', privateKey: '', publicKey: '', status: e.message });
+                response.writeHead(200, { 'Content-Type': 'application/json' })
+                response.write(JSON.stringify(returndata));
+                response.end();
+            }
+        }
+        else if (jsonContent.description == 'createNewAccountWithPublicKey') {
 
-  }).on('end', () => {
-    console.log('No more data received.');
+            try {
+                
+                eos.transaction(tr => {
+                    tr.newaccount({
+                        creator: 'cptblackbill',
+                        name: jsonContent.newAccountName,
+                        owner: jsonContent.publicKey,
+                        active: jsonContent.publicKey
+                    })
 
-    response.on('error', (err) => {
-       console.error('Error in end-function');
-       console.error(err);
+                    tr.buyrambytes({
+                        payer: 'cptblackbill',
+                        receiver: jsonContent.newAccountName,
+                        bytes: 3692
+                    })
+
+                    tr.delegatebw({
+                        from: 'cptblackbill',
+                        receiver: jsonContent.newAccountName,
+                        stake_net_quantity: jsonContent.stakeNetQuantity,
+                        stake_cpu_quantity: jsonContent.stakeCpuQuantity,
+                        transfer: 0
+                    })
+
+                    //tr.transfer({
+                    //    from: 'cptblackbill',
+                    //    to: jsonContent.newAccountName,
+                    //    quantity: jsonContent.transferEOSAmount,
+                    //    memo: 'Welcome to Cpt.BlackBill.'
+                    //})
+
+
+                }).then(function (value) {
+                    console.log('NewAccountExecuted: ' + jsonContent.publicKey);
+                    var jsonTrans1Result = JSON.parse(JSON.stringify(value));
+                    returndata.transactions.push({ transno: "1", eostransid: jsonTrans1Result['processed']['id'], publicKey: jsonContent.publicKey, status: jsonTrans1Result['processed']['receipt']['status'] });
+
+                    console.log('SendResponse');
+                    response.writeHead(200, { 'Content-Type': 'application/json' })
+                    response.write(JSON.stringify(returndata));
+                    response.end();
+                });
+
+            } catch (e) {
+                console.log('TransactionFailed');
+                returndata.transactions.push({ transno: "1", eostransid: '', publicKey: '', status: e.message });
+                response.writeHead(200, { 'Content-Type': 'application/json' })
+                response.write(JSON.stringify(returndata));
+                response.end();
+            }
+        }
+        else {
+
+            console.log('RunActions');
+
+            try {
+                eos.transaction(
+                    {
+                        actions: [
+                            {
+                                account: jsonContent.transactions[0].account,
+                                name: jsonContent.transactions[0].actionName,
+                                authorization: [{
+                                    actor: jsonContent.transactions[0].actor,
+                                    permission: 'active'
+                                }],
+                                data: jsonContent.transactions[0].binArgs
+                            }
+                        ]
+                    }).then(function (value) {
+                        console.log('TransactionExecuted:');
+                        var jsonTrans1Result = JSON.parse(JSON.stringify(value));
+                        returndata.transactions.push({ transno: "1", eostransid: jsonTrans1Result['processed']['id'], status: jsonTrans1Result['processed']['receipt']['status'] });
+
+                        console.log('SendResponse');
+                        response.writeHead(200, { 'Content-Type': 'application/json' })
+                        response.write(JSON.stringify(returndata));
+                        response.end();
+                    }
+                    );
+            } catch (e) {
+                console.log('TransactionFailed');
+                console.log(e);
+            }
+        }
+
+    }).on('end', () => {
+        console.log('No more data received.');
+
+        response.on('error', (err) => {
+            console.error('Error in end-function');
+            console.error(err);
+        });
+
+        //  response.writeHead(200, {'Content-Type': 'application/json'})
+        //  const responseBody = { 'Message':'Timeout on 15 seconds.','Data':'No action performed.' };
+        //  response.write(JSON.stringify(responseBody));
+        //  response.end();
+
     });
-
-    //  response.writeHead(200, {'Content-Type': 'application/json'})
-    //  const responseBody = { 'Message':'Timeout on 15 seconds.','Data':'No action performed.' };
-    //  response.write(JSON.stringify(responseBody));
-    //  response.end();
-
-  });
-}).listen(3000)
+}).listen(3000)// JavaScript source code
